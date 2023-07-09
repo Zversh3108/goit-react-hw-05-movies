a4a38b00bb2e036a056e4c7ff99e5ce9d06f62aa
Zversh3108 <zvershzt3108@gmail.com>
2023-07-09 02:07:33 +0200

    bugFixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Routes, Route, NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 export function Header() {
   return (
-    <header style={{ backgroundColor: '#B3E5FC', color: '#000', width: '100%', padding: '10px' }}>
-    <nav>
-      <ul style={{ display: 'flex', justifyContent: 'flex-end', listStyle: 'none', margin: 0, padding: 0 }}>
-        <li style={{ margin: '0 10px' }}>
-          <Link to="/">Home</Link>
-        </li>
-        <li style={{ margin: '0 10px' }}>
-          <Link to="/movies">Movies</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
-};
-
+    <header
+      style={{
+        backgroundColor: '#B3E5FC',
+        color: '#000',
+        width: '100%',
+        padding: '10px',
+      }}
+    >
+      <nav>
+        <ul
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            listStyle: 'none',
+            margin: 0,
+            padding: 0,
+          }}
+        >
+          <li style={{ margin: '0 10px' }}>
+            <Link to="/">Home</Link>
+          </li>
+          <li style={{ margin: '0 10px' }}>
+            <Link to="/movies">Movies</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
