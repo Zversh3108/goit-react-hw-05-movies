b4601688ef91c724edb09e96c48eb159913abedb
Zversh3108 <zvershzt3108@gmail.com>
2023-07-09 02:20:46 +0200

    addUseCallback

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import getReviewsAPI from 'services/getReviews';
 import { useParams } from 'react-router-dom';
 
 export function Reviews(props) {
   const { movieId } = useParams();
   const apiService = new getReviewsAPI();
-  const [reviews, setReviews] = useState([]);
+  const [reviews, setReviews] = use,State([]);
   const [error, setError] = useState(null);
 
-  async function fetchReviews() {
+  const fetchReviews = useCallback(async () => {
     try {
       const response = await apiService.getReviews(movieId);
       setReviews(response.results.results);
     } catch (error) {
       setError(error);
     }
-  }
+  }, [apiService, movieId]);
   useEffect(() => {
     if (!reviews) {
       return <div>Loading...</div>;
@@ -24,7 +24,7 @@ export function Reviews(props) {
 
   useEffect(() => {
     fetchReviews();
-  }, [fetchReviews ]);
+  }, [fetchReviews]);
 
   if (error) {
     return <div>Error. No reviews </div>;
